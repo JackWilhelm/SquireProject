b6e4ab6e006ba5ba34b5e3ad4f6141fe6b6ccab5
JackWilhelm <jgwilhel@ucsc.edu>
2024-11-21 18:24:48 -0800

    First commit set up

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -21,23 +21,23 @@ public class PlayerMovement : MonoBehaviour
         Vector2 botCenter = maxDistanceFromBot.transform.position;
 
         float moveX = Input.GetAxis("Horizontal");
-            float moveY = Input.GetAxis("Vertical");
+        float moveY = Input.GetAxis("Vertical");
 
-            Vector2 move = new Vector2(moveX, moveY) * moveSpeed;
+        Vector2 move = new Vector2(moveX, moveY) * moveSpeed;
 
-            playerRigidBody.velocity = move;
+        playerRigidBody.velocity = move;
 
-            Vector2 clampedPosition = playerRigidBody.position;
-            clampedPosition.x = Mathf.Clamp(clampedPosition.x, groundBounds.min.x, groundBounds.max.x);
-            clampedPosition.y = Mathf.Clamp(clampedPosition.y, groundBounds.min.y, groundBounds.max.y);
+        Vector2 clampedPosition = playerRigidBody.position;
+        clampedPosition.x = Mathf.Clamp(clampedPosition.x, groundBounds.min.x, groundBounds.max.x);
+        clampedPosition.y = Mathf.Clamp(clampedPosition.y, groundBounds.min.y, groundBounds.max.y);
 
-            Vector2 fromBotCenter = clampedPosition - botCenter;
-            if (fromBotCenter.magnitude > maxDistanceFromBot.radius) {
-                fromBotCenter.Normalize();
-                clampedPosition = botCenter + fromBotCenter * maxDistanceFromBot.radius;
-            }
+        Vector2 fromBotCenter = clampedPosition - botCenter;
+        if (fromBotCenter.magnitude > maxDistanceFromBot.radius) {
+            fromBotCenter.Normalize();
+            clampedPosition = botCenter + fromBotCenter * maxDistanceFromBot.radius;
+        }
 
-            playerRigidBody.position = clampedPosition;
+        playerRigidBody.position = clampedPosition;
     }
 
 }
