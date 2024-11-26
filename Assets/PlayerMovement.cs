b0ca21058346a71a570e553393d8814d3bbed27a
JackWilhelm <jgwilhel@ucsc.edu>
2024-11-26 14:42:43 -0800

    Movement inputs change to GetRawAxis with slipperiness changes

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,6 +8,7 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public PolygonCollider2D groundCollider;
     public CircleCollider2D maxDistanceFromBot;
+    public int footGrip = 100;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,18 +17,15 @@ public class PlayerMovement : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        Vector2 botCenter = maxDistanceFromBot.transform.position;
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
 
-        float moveX = Input.GetAxis("Horizontal");
-         float moveY = Input.GetAxis("Vertical");
-
-        Vector2 move = new Vector2(moveX, moveY) * moveSpeed;
-
-        playerRigidBody.velocity = move;
+        playerRigidBody.velocity = Vector2.Lerp(playerRigidBody.velocity, new Vector2(moveX, moveY) * moveSpeed, Time.deltaTime * footGrip);
+        
 
         Vector2 newPosition = playerRigidBody.position;
     
-
+        Vector2 botCenter = maxDistanceFromBot.transform.position;
         Vector2 fromBotCenter = newPosition - botCenter;
         if (fromBotCenter.magnitude > maxDistanceFromBot.radius) {
             fromBotCenter.Normalize();
