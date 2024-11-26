17d0131502f808628b9aae2f1b53a3df860939ac
JackWilhelm <jgwilhel@ucsc.edu>
2024-11-26 15:19:23 -0800

    Playtest dash based on mouse or movement

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -8,7 +8,13 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public PolygonCollider2D groundCollider;
     public CircleCollider2D maxDistanceFromBot;
-    public int footGrip = 100;
+    public float footGrip = 100;
+    public float dashCooldown = 2;
+    private float lastDashTime = -100f;
+    private bool isDashing = false;
+    public float dashSpeed = 20f;
+    public float dashDuration = 0.2f;
+    public bool dashMethodMouse = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,10 +26,18 @@ public class PlayerMovement : MonoBehaviour
         float moveX = Input.GetAxisRaw("Horizontal");
         float moveY = Input.GetAxisRaw("Vertical");
 
-        playerRigidBody.velocity = Vector2.Lerp(playerRigidBody.velocity, new Vector2(moveX, moveY) * moveSpeed, Time.deltaTime * footGrip);
-        
-
+        Vector2 moveInput = new Vector2(moveX, moveY).normalized;
+        if (!isDashing) {
+            playerRigidBody.velocity = Vector2.Lerp(playerRigidBody.velocity, moveInput * moveSpeed, Time.deltaTime * footGrip);
+        }
         Vector2 newPosition = playerRigidBody.position;
+
+        if (Input.GetMouseButtonDown(1) && CanDash()) {
+            Debug.Log("dash");
+            StartCoroutine(Dash(moveInput));
+        }
+
+
     
         Vector2 botCenter = maxDistanceFromBot.transform.position;
         Vector2 fromBotCenter = newPosition - botCenter;
@@ -43,5 +57,27 @@ public class PlayerMovement : MonoBehaviour
         Vector2 playerPosition = playerRigidBody.position;
         Vector2 closestPoint = groundCollider.ClosestPoint(playerPosition);
         playerRigidBody.position = closestPoint;
+    }
+
+    bool CanDash() {
+        return Time.time >= lastDashTime + dashCooldown;
+    }
+
+    private IEnumerator Dash(Vector2 moveDirection) {
+        isDashing = true;
+        lastDashTime = Time.time;
+
+        if (dashMethodMouse) {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+            moveDirection = (mousePosition - (Vector2)transform.position).normalized;
+        }
+        Vector2 dashVelocity = moveDirection * dashSpeed;
+        playerRigidBody.velocity = dashVelocity;
+        
+
+        yield return new WaitForSeconds(dashDuration);
+
+        isDashing = false;
     }
 }
